7aa1868239f727035d06f2962d130e8da812761b
Angelica Garcia <70681219+YessBlack@users.noreply.github.com>
2026-08-24 10:19:16 -0500

    add structure project

---
+++ src/co/generation/clinica/Main.java
@@ -0,0 +1,16 @@
+package co.generation.clinica;
+
+import co.generation.clinica.datos.DatosCSV;
+import co.generation.clinica.service.ClinicaService;
+
+public class Main {
+    public static void main(String[] args) {
+        // Secuencia en main():
+        ClinicaService servicio = new ClinicaService();
+        // DatosCSV.cargar(servicio);
+        // bucle while con Scanner para el menú...
+        // Al salir (opción 0):
+        // DatosCSV.guardar(servicio);
+        System.out.println("Hasta pronto. Datos guardados.");
+    }
+}
